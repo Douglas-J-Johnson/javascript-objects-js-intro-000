9e3740e820243d5ffbc3b7ad93c6ceeb7f968d6f
Douglas Johnson <douglas.j.johnson@gmail.com>
2019-12-01 01:11:36 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,1 +1,6 @@
 var playlist = {'Michael Jackson':"Thriller"}
+
+function updatePlaylist(playlist, artisName, songTitle) {
+  playlist = Object.assign(playlist,{artistName: songTitle})
+  return playlist
+}
